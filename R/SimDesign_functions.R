b0bd1a7791211087a95aa3735e2a22870de9efff
Phil Chalmers <rphilip.chalmers@gmail.com>
2015-10-15 13:29:14 -0400

    write file option

--- R/SimDesign_functions.R
+++ R/SimDesign_functions.R
@@ -4,6 +4,10 @@
 #' to run simulations, complete with the correct inputs and class of outputs. Use this at the start
 #' when defining your simulation.
 #'
+#' @param filename a character vector indicating whether the output should be saved to two respective files
+#'   containing the simulation design and the functional components, respectively. Using this option
+#'   is generally the recommended approach when designing simulations
+#'
 #' @param main logical; include a skeleton of the \code{main} function?
 #'
 #' @aliases SimDesign_functions
@@ -14,10 +18,13 @@
 #'
 #' SimDesign_functions()
 #'
-SimDesign_functions <- function(main = FALSE){
+#' # write output files (not run)
+#' # SimDesign_functions('mysim')
+#'
+SimDesign_functions <- function(filename = NULL, main = FALSE){
+    if(!is.null(filename)) sink(paste0(filename, '-functions.R'))
     cat('\n#-------------------------------------------------------------------')
-    cat('\n### Define essential simulation functions. It may be helpful to place these')
-    cat('\n### functions in a seperate R file and source() them in')
+    cat('\n### Define essential simulation functions')
     cat('\n\nGenerate <- function(condition, fixed_design_elements = NULL) {')
     cat('\n    # Define data generation code ...\n')
     cat('\n    # Return a vector, matrix, data.frame, or list')
@@ -32,14 +39,21 @@ SimDesign_functions <- function(main = FALSE){
     if(main){
         cat('main <- ', paste0(deparse(main), '\n'))
     }
-    cat('\n#-------------------------------------------------------------------')
-
+    if(!is.null(filename)) sink()
+    if(!is.null(filename)){
+        sink(paste0(filename, '.R'))
+        cat('\n#-------------------------------------------------------------------')
+        cat('\n\n### Source in essential functions')
+        cat('\n# setwd(\"', getwd(), '\")', sep='')
+        cat('\nsource(\"', paste0(filename, '.R\"'), ')', sep='')
+    }
     cat('\n\n### Define design conditions and number of replications')
-    cat('\n# Design <- expand.grid(condition1, condition2, ...)')
-    cat('\n# replications <- 1000')
-
+    cat('\nDesign <- expand.grid(condition1, condition2, ...)')
+    cat('\nreplications <- 1000')
     cat('\n\n### Run the simulation')
     cat('\nresults <- runSimulation(Design, replications, ')
     cat('\n    generate=Generate, analyse=Analyse, summarise=Summarise, edit=\'none\')')
     cat('\n\n')
+    if(!is.null(filename)) sink()
+    invisible()
 }
