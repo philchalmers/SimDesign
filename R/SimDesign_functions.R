157fd37c21392bd302aaf50d5079b24e5a11a45b
Phil Chalmers <rphilip.chalmers@gmail.com>
2015-12-08 15:32:55 -0500

    reorganise

--- R/SimDesign_functions.R
+++ R/SimDesign_functions.R
@@ -20,10 +20,11 @@
 #' @examples
 #'
 #' SimDesign_functions()
+#' SimDesign_functions(comments=FALSE) #without comments
 #'
 #'\dontrun{
 #'
-#' # write output files
+#' # write output to two files
 #' SimDesign_functions('mysim')
 #'
 #' # write output files to a single file without comments
@@ -31,51 +32,61 @@
 #' }
 #'
 SimDesign_functions <- function(filename = NULL, comments = TRUE, singlefile = FALSE){
-    if(singlefile){
-        if(!is.null(filename)) sink(paste0(filename, '.R'))
-    } else {
-        if(!is.null(filename)) sink(paste0(filename, '-functions.R'))
-    }
-    cat('#-------------------------------------------------------------------')
-    if(comments) cat('\n### Define essential simulation functions')
-    cat('\n\nGenerate <- function(condition, fixed_objects = NULL) {')
-    if(comments) cat('\n    # Define data generation code ...\n')
-    if(comments) cat('\n    # Return a vector, matrix, data.frame, or list')
-    cat('\n    dat <- data.frame()')
-    cat('\n    dat\n}')
-    cat('\n\n')
-    cat('Analyse <- function(condition, dat, fixed_objects = NULL, parameters = NULL) {')
-    if(comments) cat('\n    # Run statistical analyses of interest ... \n')
-    if(comments) cat('\n    # Return a named vector or list')
-    cat('\n    ret <- c(stat1=NaN, stat2=NaN)\n    ret\n}')
-    cat('\n\n')
-    cat('Summarise <- function(condition, results, fixed_objects = NULL, parameters_list = NULL) {')
-    if(comments) cat('\n    # Summarise the simulation results ...\n')
-    if(comments) cat('\n    # Return a named vector of results')
-    cat('\n    ret <- c(bias=NaN, RMSE=NaN)\n    ret\n}\n\n')
-
-    if(!singlefile)
-        if(!is.null(filename)) sink()
-    if(!is.null(filename)){
-        if(!singlefile) sink(paste0(filename, '.R'))
+    LINE <- function()
         cat('#-------------------------------------------------------------------\n')
+    HEAD <- function(){
+        LINE()
         cat('\nlibrary(SimDesign)\n')
         if(comments)
-            if(!singlefile) cat('\n### Source in essential functions')
-        cat('\n# setwd(\"', getwd(), '\")', sep='')
-        if(!singlefile) cat('\nsource(\"', paste0(filename, '-functions.R\"'), ')', sep='')
-        cat('\n')
-    } else {
-        cat('#-------------------------------------------------------------------\n')
-        cat('\nlibrary(SimDesign)\n')
+            cat('\n### Define design conditions')
+        cat('\nDesign <- expand.grid(condition1 = NA,
+                      condition2 = NA)\n\n')
     }
-    if(comments) cat('\n### Define design conditions and number of replications')
-    cat('\nDesign <- expand.grid(condition1, condition2)')
-    cat('\nReplications <- 1000\n')
-    if(comments) cat('\n### Run the simulation')
-    cat('\nresults <- runSimulation(design=Design, replications=Replications, ')
-    cat('\n    generate=Generate, analyse=Analyse, summarise=Summarise, edit=\'none\')')
-    cat('\n\n')
-    if(!is.null(filename)) sink()
+
+    FUNCTIONS <- function(){
+        LINE()
+        if(comments) cat('\n### Define essential simulation functions\n')
+        cat('\nGenerate <- function(condition, fixed_objects = NULL) {')
+        if(comments) cat('\n    # Define data generation code ...\n')
+        if(comments) cat('\n    # Return a vector, matrix, data.frame, or list')
+        cat('\n    dat <- data.frame()')
+        cat('\n    dat\n}')
+        cat('\n\n')
+        cat('Analyse <- function(condition, dat, fixed_objects = NULL, parameters = NULL) {')
+        if(comments) cat('\n    # Run statistical analyses of interest ... \n')
+        if(comments) cat('\n    # Return a named vector or list')
+        cat('\n    ret <- c(stat1 = NaN, stat2 = NaN)\n    ret\n}')
+        cat('\n\n')
+        cat('Summarise <- function(condition, results, fixed_objects = NULL, parameters_list = NULL) {')
+        if(comments) cat('\n    # Summarise the simulation results ...\n')
+        if(comments) cat('\n    # Return a named vector of results')
+        cat('\n    ret <- c(bias = NaN, RMSE = NaN)\n    ret\n}\n\n')
+    }
+
+    TAIL <- function(){
+        LINE()
+        if(comments) cat('\n### Run the simulation\n')
+        cat('\nresults <- runSimulation(design=Design, replications=Replications, ')
+        cat('\n    generate=Generate, analyse=Analyse, summarise=Summarise, edit=\'none\')')
+        cat('\n\n')
+    }
+
+    #main
+    if(is.null(filename) || singlefile){
+        if(singlefile)
+            if(!is.null(filename)) sink(paste0(filename, '.R'))
+        HEAD()
+        FUNCTIONS()
+        TAIL()
+        if(!is.null(filename)) sink()
+    } else {
+        sink(paste0(filename, '.R'))
+        HEAD()
+        TAIL()
+        sink()
+        sink(paste0(filename, '-functions.R'))
+        FUNCTIONS()
+        sink()
+    }
     invisible()
 }
