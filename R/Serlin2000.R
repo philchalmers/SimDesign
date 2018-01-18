f035a636f4b013c1b2dce8ebf2e8f4694751b060
Phil Chalmers <rphilip.chalmers@gmail.com>
2018-01-17 22:32:04 -0500

    support multiple inputs

--- R/Serlin2000.R
+++ R/Serlin2000.R
@@ -6,20 +6,18 @@
 #' generate critical values (similar to confidence intervals, but define a fixed
 #' window of robustness).
 #'
-#' @param p empirical detection rate
-#'
-#' @param lower lower bound of the rejection region
-#'
-#' @param upper upper bound of the rejection region
+#' @param p a vector containing the empirical detection rate(s) to be tested
 #'
 #' @param R number of replications used in the simulation
 #'
 #' @param alpha Type I error rate (e.g., often set to .05)
 #'
-#' @param CI confidence interval for \code{alpha} as a proportion. Default of 0.95
-#'   indicates a 95% interval
+#' @param delta symmetric robustness interval around \code{alpha} (e.g., a value
+#'   of .01 when \code{alpha = .05} would test the robustness window .04-.06)
 #'
-#' @aliases Serlin2000
+#' @param CI confidence interval for \code{alpha} as a proportion. Default of 0.95
+#'   indicates a 95\% interval
+#'
 #' @references
 #' Serlin, R. C. (2000). Testing for Robustness in Monte Carlo Studies.
 #' \emph{Psychological Methods, 5}, 230-240.
@@ -28,20 +26,22 @@
 #' Carlo simulation. \code{Journal of Statistics Education, 24}(3), 136-156.
 #' \doi{10.1080/10691898.2016.1246953}
 #'
-#' @export Serlin2000
+#' @export
 #'
 #' @author Phil Chalmers \email{rphilip.chalmers@@gmail.com}
 #'
 #' @examples
 #'
 #' # Cochran's criteria at alpha = .05 (i.e., 0.5 +- .01), assuming N = 2000
-#' Serlin2000(delta = .01, p = .051, alpha = .05, R = 2000)
+#' Serlin2000(p = .051, alpha = .05, delta = .01, R = 2000)
 #'
 #' # Bradley's liberal criteria given p = .06 and .076, assuming N = 1000
-#' Serlin2000(delta = .025, p = .060, alpha = .05, R = 1000)
-#' Serlin2000(delta = .025, p = .076, alpha = .05, R = 1000)
+#' Serlin2000(p = .060, alpha = .05, delta = .025, R = 1000)
+#' Serlin2000(p = .076, alpha = .05, delta = .025, R = 1000)
 #'
-Serlin2000 <- function(delta, p, alpha, R, CI = .95){
+#' # multiple p-values
+#' Serlin2000(p = c(.05, .06, .07), alpha = .05, delta = .025, R = 1000)
+Serlin2000 <- function(p, alpha, delta, R, CI = .95){
     sigma2 <- alpha * (1 - alpha) / R
     crit <- (abs(p - alpha) - delta) / sqrt(sigma2)
     crit_p <- pnorm(abs(crit), lower.tail = FALSE) # one-tailed test
@@ -57,7 +57,8 @@ Serlin2000 <- function(delta, p, alpha, R, CI = .95){
     z_crit <- abs(qnorm((1 - CI)))
     CV2 <- try(uniroot(fn, interval = c(alpha, 1 - 1e-10))$root, TRUE)
     if(is(CV2, 'try-error')) CV2 <- NA
-    ret <- data.frame("z(|p - alpha| - delta))" = crit,
+    ret <- data.frame("p" = p,
+                      "z(|p-a| - d))" = crit,
                       "Pr(>|z|)" = crit_p, check.names = FALSE,
                       "robust" = robust,
                       "CV1" = CV1,
