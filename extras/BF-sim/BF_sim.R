2fd786bb12d62541d3dbfbdfbb6259ca8a9791d3
philchalmers <rphilip.chalmers@gmail.com>
2019-06-09 11:10:05 -0400

    update examples

--- extras/BF-sim/BF_sim.R
+++ extras/BF-sim/BF_sim.R
@@ -2,7 +2,8 @@
 #
 # Robust Tests for the Equality of Variances
 # Author(s): Morton B. Brown and Alan B. Forsythe
-# Source: Journal of the American Statistical Association, Vol. 69, No. 346 (Jun., 1974), pp. 364-367
+# Source: Journal of the American Statistical Association,
+#   Vol. 69, No. 346 (Jun., 1974), pp. 364-367
 # Published by: American Statistical Association
 
 #-------------------------------------------------------------------
@@ -10,6 +11,9 @@
 
 # devtools::install_github('philchalmers/SimDesign')
 # SimDesign::SimFunctions('BF-sim', comment=FALSE)
+
+# include extra user-defined functions
+source('BF_sim_custom-functions.R')
 
 #-------------------------------------------------------------------
 # Define the design conditions
@@ -38,14 +42,55 @@ Design$sample_size[pick2] <- 30
 print(Design)
 
 #-------------------------------------------------------------------
+
+Generate <- function(condition, fixed_objects = NULL) {
+    N <- with(condition, sample_size)
+    v <- with(condition, var_ratio)
+    sd1 <- ifelse(v <= 1, 1, sqrt(v))
+    sd2 <- ifelse(v <= 1, sqrt(1/v), 1)
+    if(with(condition, groups_equal)){
+        N1 <- N2 <- N/2
+    } else {
+        N1 <- N * 1/3
+        N2 <- N - N1
+    }
+    dat <- switch(as.character(condition$distribution),
+                  Gaussian = c(rnorm(N1, sd = sd1), rnorm(N2, sd = sd2)),
+                  t4 = c(rt(N1, df = 4), rt(N2, df = 4)),
+                  Chi4 = c(rchisq(N1, df = 4), rchisq(N2, df = 4)),
+                  Cauchy = c(rcauchy(N1), rcauchy(N2))
+    )
+    dat <- data.frame(DV=dat, group=c(rep('G1', N1), rep('G2', N2)))
+    dat
+}
+
+Analyse <- function(condition, dat, fixed_objects = NULL) {
+    DV <- dat$DV
+    group <- dat$group
+    F_test <- var.test(DV ~ group)$p.value
+    Jacknife <- Jacknife_vartest(DV, group)$p.value
+    Layard <- Layard_vartest(DV, group)$p.value
+    W50 <- levene.test(DV, group=group, location = 'median')$p.value
+    W10 <- levene.test(DV, group=group, location = 'trim.mean',
+                       trim.alpha = .1)$p.value
+    Levene <- levene.test(DV, group=group, location = 'mean')$p.value
+    ret <- c(F=F_test, Jacknife=Jacknife, Layard=Layard,
+             Levene=Levene, W10=W10, W50=W50)
+    ret
+}
+
+Summarise <- function(condition, results, fixed_objects = NULL) {
+    ret <- EDR(results, alpha = .05)
+    ret
+}
+
+#-------------------------------------------------------------------
 # Run the MCS
 
-source('BF_sim_custom-functions.R')
-source('BF_sim-functions.R')
-
 results <- runSimulation(design=Design, replications=1000, parallel=TRUE,
-                         generate=Generate, analyse=Analyse, summarise=Summarise,
-                         packages='lawstat', filename='BF_simulation')
+                         generate=Generate, analyse=Analyse,
+                         summarise=Summarise, packages='lawstat',
+                         filename='BF_simulation')
 View(results)
 
 TypeI <- subset(results, var_ratio == 1)
--- extras/Nordstokke_Zumbo2007/Nordstokke_Zumbo2007.R
+++ extras/Nordstokke_Zumbo2007/Nordstokke_Zumbo2007.R
@@ -6,16 +6,46 @@ Design <- expand.grid(skewness = 0:3,
                       sample_size = c(24,48,96),
                       sample_size_ratio = 1:3,
                       var_ratio = c(5:1, 1/2, 1/3, 1/4, 1/5))
-
+head(Design)
 dim(Design)
-
-# setwd("/home/phil/Desktop")
-source("Nordstokke_Zumbo2007-functions.R")
 
 #-------------------------------------------------------------------
 
+Generate <- function(condition, fixed_objects = NULL) {
+    N <- with(condition, sample_size)
+    R <- with(condition, sample_size_ratio)
+    V <- with(condition, var_ratio)
+    S <- with(condition, skewness)
+    N1 <- N * R / (R + 1)
+    N2 <- N - N1
+    df <- switch(as.character(S),
+                 '0' = 10000,
+                 '1' = 7.4,
+                 '2' = 2.2,
+                 '3' = .83)
+    DV <- c(rchisq(N1, df) * sqrt(V), rchisq(N2, df))
+    dat <- data.frame(DV=DV, group=c(rep('G1', N1), rep('G2', N2)))
+    dat
+}
+
+Analyse <- function(condition, dat, fixed_objects = NULL) {
+    BF <- levene.test(dat$DV, dat$group, location = 'median')
+    Levene <- levene.test(dat$DV, dat$group, location = 'mean')
+    F_test <- var.test(DV ~ group, dat)
+    ret <- c(BF=BF$p.value, Levene=Levene$p.value, F=F_test$p.value)
+    ret
+}
+
+Summarise <- function(condition, results, fixed_objects = NULL) {
+    ret <- EDR(results, alpha = .05)
+    ret
+}
+
+#-------------------------------------------------------------------
+
+# save results to the drive
 runSimulation(design=Design, replications=5000, packages = 'lawstat',
-    generate=Generate, analyse=Analyse, summarise=Summarise, edit='none', parallel=TRUE,
+    generate=Generate, analyse=Analyse, summarise=Summarise, parallel=TRUE,
     save=TRUE, filename='Nordstokke_Zumbo2007')
 
 results <- readRDS('Nordstokke_Zumbo2007.rds')
