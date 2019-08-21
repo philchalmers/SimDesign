40c53d01b30f649ab3d84abc312037fc5d7cd4f0
Phil Chalmers <rphilip.chalmers@gmail.com>
2019-08-21 13:23:53 -0400

    update BF script

--- extras/BF-sim/BF_sim.R
+++ extras/BF-sim/BF_sim.R
@@ -20,53 +20,48 @@ source('BF_sim_custom-functions.R')
 
 library(SimDesign)
 
-groups_equal <- c(TRUE, FALSE)
-var_ratio <- c(4, 2, 1, 1/2, 1/4)
-sample_size <- c(80, 20)
-distributions <- c('Gaussian', 't4', 'Chi4', 'Cauchy')
-Design <- expand.grid(var_ratio=var_ratio,
-                      sample_size=sample_size,
-                      groups_equal=groups_equal,
-                      distribution=distributions)
+Design <- expand.grid(var_ratio=c(4, 2, 1, 1/2, 1/4),
+                      N=c(80, 20),
+                      groups_equal=c(TRUE, FALSE),
+                      distribution=c('Gaussian', 't4', 'Chi4', 'Cauchy'))
 
-#remove redundent or not-applicable rows
-Design <- subset(Design, !(groups_equal & var_ratio < 1))
-Design <- subset(Design, !(distribution != 'Gaussian' & var_ratio != 1))
+# remove redundent or not-applicable rows
+Design <- subset(Design, !(groups_equal & var_ratio < 1) |
+                     !(distribution != 'Gaussian' & var_ratio != 1))
 
 # Brown and Forsythe use different sample sizes when groups were unequal
-pick1 <- with(Design, !groups_equal & sample_size == 80)
-pick2 <- with(Design, !groups_equal & sample_size == 20)
-Design$sample_size[pick1] <- 60
-Design$sample_size[pick2] <- 30
+pick1 <- with(Design, !groups_equal & N == 80)
+pick2 <- with(Design, !groups_equal & N == 20)
+Design$N[pick1] <- 60
+Design$N[pick2] <- 30
 
-print(Design)
+Design
 
 #-------------------------------------------------------------------
 
 Generate <- function(condition, fixed_objects = NULL) {
-    N <- with(condition, sample_size)
-    v <- with(condition, var_ratio)
-    sd1 <- ifelse(v <= 1, 1, sqrt(v))
-    sd2 <- ifelse(v <= 1, sqrt(1/v), 1)
-    if(with(condition, groups_equal)){
+    Attach(condition)
+    sd1 <- ifelse(var_ratio <= 1, 1, sqrt(var_ratio))
+    sd2 <- ifelse(var_ratio <= 1, sqrt(1/var_ratio), 1)
+    if(groups_equal){
         N1 <- N2 <- N/2
     } else {
         N1 <- N * 1/3
         N2 <- N - N1
     }
-    dat <- switch(as.character(condition$distribution),
+    dv <- switch(as.character(distribution),
                   Gaussian = c(rnorm(N1, sd = sd1), rnorm(N2, sd = sd2)),
                   t4 = c(rt(N1, df = 4), rt(N2, df = 4)),
                   Chi4 = c(rchisq(N1, df = 4), rchisq(N2, df = 4)),
                   Cauchy = c(rcauchy(N1), rcauchy(N2))
     )
-    dat <- data.frame(DV=dat, group=c(rep('G1', N1), rep('G2', N2)))
+    dat <- data.frame(DV=dv,
+                      group=c(rep('G1', N1), rep('G2', N2)))
     dat
 }
 
 Analyse <- function(condition, dat, fixed_objects = NULL) {
-    DV <- dat$DV
-    group <- dat$group
+    Attach(dat)
     F_test <- var.test(DV ~ group)$p.value
     Jacknife <- Jacknife_vartest(DV, group)$p.value
     Layard <- Layard_vartest(DV, group)$p.value
@@ -91,10 +86,10 @@ results <- runSimulation(design=Design, replications=1000, parallel=TRUE,
                          generate=Generate, analyse=Analyse,
                          summarise=Summarise, packages='lawstat',
                          filename='BF_simulation')
-View(results)
+results
 
 TypeI <- subset(results, var_ratio == 1)
-View(TypeI)
+TypeI
 
 Power <- subset(results, var_ratio != 1)
-View(Power)
+Power
