ac1493e2522b07a7436d0d6a9b6ebe8955613d34
philchalmers <rphilip.chalmers@gmail.com>
2021-03-22 12:40:16 -0400

    fix doc

--- R/rtruncate.R
+++ R/rtruncate.R
@@ -1,28 +1,31 @@
 #' Generate a random set of values within a truncated range
 #'
 #' Function generates data given a supplied random number generating function that
-#' are constructed to fall within a particular range. Sampled values outside this range
-#' are discarded and re-sampled until the desired criteria has been met.
+#' are constructed to fall within a particular range. Sampled values outside this
+#' range are discarded and re-sampled until the desired criteria has been met.
 #'
 #' In simulations it is often useful to draw numbers from truncated distributions
-#' rather than across the full theoretical range. For instance, sampling parameters within
-#' the range [-4,4] from a normal distribution. The \code{rtruncate} function has been
-#' designed to accept any sampling function, where the first argument is the number of
-#' values to sample, and will draw values iteratively until the number of values
-#' within the specified bound are obtained. In situations where it is unlikely for the bounds
-#' to be located (e.g., sampling from a normal distribution where all values are within [-10,-6])
-#' then the sampling scheme will throw an error if too many re-sampling executions are required
-#' (default will stop if more that 100 calls to \code{rfun} are required).
+#' rather than across the full theoretical range. For instance, sampling parameters
+#' within the range [-4,4] from a normal distribution. The \code{rtruncate}
+#' function has been designed to accept any sampling function, where the first
+#' argument is the number of values to sample, and will draw values iteratively
+#' until the number of values within the specified bound are obtained.
+#' In situations where it is unlikely for the bounds to be located
+#' (e.g., sampling from a standard normal distribution where all values are
+#' within [-10,-6]) then the sampling scheme will throw an error if too many
+#' re-sampling executions are required (default will stop if more that 100
+#' calls to \code{rfun} are required).
 #'
-#' @param n number of observations to generate. This should be the first argument passed
-#'   to \code{rfun}
+#' @param n number of observations to generate. This should be the first argument
+#'   passed to \code{rfun}
 #'
-#' @param range a numeric vector of length two, where the first element indicates the
-#'   lower bound and the second the upper bound. When values are generated outside these
-#'   two bounds then data are redrawn until the bounded criteria is met. When the
-#'   output of \code{rfun} is a matrix then this input can be specified as a matrix with
-#'   two rows, where each the first row corresponds to the lower bound and the second row
-#'   the upper bound for each generated column in the output
+#' @param range a numeric vector of length two, where the first element
+#'   indicates the lower bound and the second the upper bound. When values are
+#'   generated outside these two bounds then data are redrawn until the bounded
+#'   criteria is met. When the output of \code{rfun} is a matrix then this input
+#'   can be specified as a matrix with two rows, where each the first row
+#'   corresponds to the lower bound and the second row the upper bound for
+#'   each generated column in the output
 #'
 #' @param rfun a function to generate random values. Function can return
 #'   a numeric/integer vector or matrix, and additional arguments
@@ -30,9 +33,10 @@
 #'
 #' @param ... additional arguments to be passed to \code{rfun}
 #'
-#' @param redraws the maximum number of redraws to take before terminating the iterative
-#'   sequence. This is in place as a safety in case the \code{range} is too small given the
-#'   random number generator, causing too many consecutive rejections. Default is 100
+#' @param redraws the maximum number of redraws to take before terminating the
+#'   iterative sequence. This is in place as a safety in case the \code{range}
+#'   is too small given the random number generator, causing too many
+#'   consecutive rejections. Default is 100
 #'
 #' @return either a numeric vector or matrix, where all values are within the
 #'   desired \code{range}
@@ -40,13 +44,14 @@
 #' @seealso \code{\link{runSimulation}}
 #' @references
 #'
-#' Chalmers, R. P., & Adkins, M. C.  (2020). Writing Effective and Reliable Monte Carlo Simulations
-#' with the SimDesign Package. \code{The Quantitative Methods for Psychology, 16}(4), 248-280.
-#' \doi{10.20982/tqmp.16.4.p248}
+#' Chalmers, R. P., & Adkins, M. C.  (2020). Writing Effective and Reliable
+#'   Monte Carlo Simulations with the SimDesign Package.
+#'   \code{The Quantitative Methods for Psychology, 16}(4), 248-280.
+#'    \doi{10.20982/tqmp.16.4.p248}
 #'
-#' Sigal, M. J., & Chalmers, R. P. (2016). Play it again: Teaching statistics with Monte
-#' Carlo simulation. \code{Journal of Statistics Education, 24}(3), 136-156.
-#' \doi{10.1080/10691898.2016.1246953}
+#' Sigal, M. J., & Chalmers, R. P. (2016). Play it again: Teaching statistics
+#'   with Monte Carlo simulation. \code{Journal of Statistics Education, 24}(3),
+#'   136-156. \doi{10.1080/10691898.2016.1246953}
 #'
 #' @author Phil Chalmers \email{rphilip.chalmers@@gmail.com}
 #'
@@ -96,6 +101,7 @@ rtruncate <- function(n, rfun, range, ..., redraws = 100L){
         draw <- draw + 1L
     }
     if(draw == redraws)
-        stop("rtruncate() redrew data too often and was terminated", call.=FALSE)
+        stop("rtruncate() redrew data too often and was terminated",
+             call.=FALSE)
     ret
 }
