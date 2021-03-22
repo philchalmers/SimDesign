2d8ab9d3096f6dec579138cacb457b3d49bc78c0
philchalmers <rphilip.chalmers@gmail.com>
2021-03-22 00:59:10 -0400

    assume things are vectorized

--- R/rejectionSampling.R
+++ R/rejectionSampling.R
@@ -1,15 +1,16 @@
-#' Rejection sampling (i.e., accept-reject method) to draw samples from difficult probability density functions
+#' Rejection sampling (i.e., accept-reject method)
 #'
-#' This function supports the rejection sampling (i.e., accept-reject) approach to drawing values
-#' from seemingly difficult, and potentially non-normed, probability density functions by
-#' sampling values from a more manageable proxy distribution. This function is
-#' optimized to work efficiently when the defined functions are vectorized; otherwise,
-#' the accept-reject algorithm will loop over candidate sample-draws in isolation. Note that by
-#' default all relavent density functions assume that the functions return log-densities for
-#' better numerical behaviour.
+#' This function supports the rejection sampling (i.e., accept-reject) approach
+#' to drawing values from seemingly difficult, and potentially non-normed,
+#' (probability) density functions by sampling values from a more manageable
+#' proxy distribution. This function is optimized to work efficiently when
+#' the defined functions are vectorized; otherwise, the accept-reject algorithm
+#' will loop over candidate sample-draws in isolation. Note that by
+#' default all relevant density functions assume that the functions return
+#' log-densities for better numerical behaviour.
 #'
-#' The accept-reject algorithm is a flexible approach to obtaining i.i.d.'s from a
-#' difficult to sample from probability density function (pdf) where either the
+#' The accept-reject algorithm is a flexible approach to obtaining i.i.d.'s from
+#' a difficult to sample from probability density function (pdf) where either the
 #' transformation method fails or inverse of the cumulative distribution function
 #' is too difficult to manage. The algorithm does so by sampling from
 #' a more "well-behaved" proxy distribution (with identical support, up to some
@@ -22,24 +23,27 @@
 #' @param n number of samples to draw
 #'
 #' @param df the desired (potentially un-normed) probability density function to
-#'   draw samples from. Must be in the form of a \code{function} with a single input
-#'   corresponding to the values sampled from \code{rg}. By default this is assumed to be
-#'   in log-density scale
+#'   draw samples from. Must be in the form of a \code{function} with a single
+#'   input corresponding to the values sampled from \code{rg}. By default this
+#'   is assumed to be in log-density scale. Function is assumed to be vectorized
+#'    (if not, see \code{\link{Vectorize}})
 #'
 #' @param dg the proxy (potentially un-normed) probability density function to
 #'   draw samples from in lieu of drawing samples from \code{df}. The support for
 #'   this density function should be the same as \code{df}
 #'   (i.e., when \code{df(x) > 0} then \code{dg(x) > 0}).
 #'   Must be in the form of a \code{function} with a single input
-#'   corresponding to the values sampled from \code{rg}. By default this is assumed to be
-#'   in log-density scale
+#'   corresponding to the values sampled from \code{rg}. By default this is
+#'   assumed to be in log-density scale. Function is assumed to be
+#'   vectorized (if not, see \code{\link{Vectorize}})
 #'
-#' @param rg the proxy random number generation function, associated with \code{dg},
-#'   used to draw samples from in lieu of drawing samples from \code{df}.
-#'   Must be in the form of a \code{function} with a single input
-#'   corresponding to the number of values to draw, while the output can either
-#'   be a vector or a matrix (if a matrix, each independent observation must be stored in
-#'   a unique row)
+#' @param rg the proxy random number generation function, associated with
+#'   \code{dg}, used to draw samples from in lieu of drawing samples from \code{df}.
+#'   Must be in the form of a \code{function} with a single input corresponding
+#'   to the number of values to draw, while the output can either be a vector
+#'   or a matrix (if a matrix, each independent observation must be stored in
+#'   a unique row). Function is assumed to be vectorized (if not, see
+#'   \code{\link{Vectorize}})
 #'
 #' @param M the upper-bound of the ratio of probability density functions to help
 #'   minimize the number of discarded draws. By default, \code{M} is computed
@@ -47,13 +51,14 @@
 #'   When both \code{df} and \code{dg} are true probability density functions
 #'   (i.e., integrate to 1) then the acceptance probability is equal to 1/M
 #'
-#' @param interval interval to search within via the \code{\link{optimize}} function. If
-#'   not specified a sample of 5000 values from the \code{rg} function definition will be
+#' @param interval interval to search within via the \code{\link{optimize}}
+#'   function. If not specified a sample of 5000 values from the \code{rg}
+#'   function definition will be
 #'   collected, and the min/max will be obtained via this random sample
 #'
-#' @param method when M is missing the optimization of M is done either by finding the
-#'   mode of the log-density values (\code{"optimize"}) or by using the
-#'   "Empirical Supremum Rejection Sampling"
+#' @param method when M is missing the optimization of M is done either by
+#'   finding the mode of the log-density values (\code{"optimize"}) or by
+#'   using the "Empirical Supremum Rejection Sampling"
 #'   method (\code{"ESRS"})
 #'
 #' @param vectorized logical; have the input function been vectorized (i.e., do they
@@ -79,16 +84,17 @@
 #'
 #' @references
 #'
-#' Caffo, B. S., Booth, J. G., and Davison, A. C. (2002). Empirical supremum rejection sampling.
-#'   \code{Biometrika}, 89, 745--754.
+#' Caffo, B. S., Booth, J. G., and Davison, A. C. (2002). Empirical supremum
+#'   rejection sampling. \code{Biometrika}, 89, 745--754.
 #'
-#' Chalmers, R. P., & Adkins, M. C.  (2020). Writing Effective and Reliable Monte Carlo Simulations
-#' with the SimDesign Package. \code{The Quantitative Methods for Psychology, 16}(4), 248-280.
-#' \doi{10.20982/tqmp.16.4.p248}
+#' Chalmers, R. P., & Adkins, M. C.  (2020). Writing Effective and Reliable
+#'   Monte Carlo Simulations with the SimDesign Package.
+#'   \code{The Quantitative Methods for Psychology, 16}(4), 248-280.
+#'   \doi{10.20982/tqmp.16.4.p248}
 #'
-#' Sigal, M. J., & Chalmers, R. P. (2016). Play it again: Teaching statistics with Monte
-#' Carlo simulation. \code{Journal of Statistics Education, 24}(3), 136-156.
-#' \doi{10.1080/10691898.2016.1246953}
+#' Sigal, M. J., & Chalmers, R. P. (2016). Play it again: Teaching statistics
+#'   with Monte Carlo simulation. \code{Journal of Statistics Education, 24}(3),
+#'   136-156. \doi{10.1080/10691898.2016.1246953}
 #'
 #'
 #' @export
@@ -156,18 +162,6 @@
 #' ndfn <- function(x) dfn(x, log=FALSE) / C
 #' curve(ndfn, col='red', lwd=2, add=TRUE)
 #'
-#' # same as above, but df not vectorized (much slower)
-#' dfn2 <- function(x, log=TRUE){
-#'     ret <- if(x <= .5) dnorm(x)
-#'     else if(x > .5) dnorm(x) + dchisq(x, df = 2)
-#'     if(log) ret <- log(ret)
-#'     ret
-#' }
-#' system.time(dat2 <-
-#'    rejectionSampling(100000, df=dfn2, dg=dgn, rg=rgn, M=M,
-#'        vectorized=FALSE))
-#' system.time(dat <-
-#'    rejectionSampling(100000, df=dfn, dg=dgn, rg=rgn, M=M))
 #'
 #' #-----------------------------------------------------
 #' # multivariate distribution
@@ -186,7 +180,7 @@
 #'
 rejectionSampling <- function(n, df, dg, rg, M, method = 'optimize',
                               interval = NULL, logfuns = TRUE,
-                              vectorized = TRUE, maxM = 1e5, parstart = rg(1L)) {
+                              maxM = 1e5, parstart = rg(1L)) {
     stopifnot(!missing(rg))
     stopifnot(!missing(dg))
     stopifnot(!missing(df))
@@ -230,8 +224,8 @@ rejectionSampling <- function(n, df, dg, rg, M, method = 'optimize',
         if(while_iter == 30L)
             if(n.remaining == n)
                 stop("No sucessful draws computed after 30 iterations")
-        y <- if(vectorized) rg(n.remaining) else rg(1L)
-        u <- if(vectorized) runif(n.remaining, 0, 1) else runif(1L, 0, 1)
+        y <- rg(n.remaining)
+        u <- runif(n.remaining, 0, 1)
         pick <- if(multipar){
             y <- matrix(y, ncol = npar)
             log_diff <-
@@ -254,7 +248,7 @@ rejectionSampling <- function(n, df, dg, rg, M, method = 'optimize',
         }
         if(ESRS){
             iter <- iter + 1L
-            if( (vectorized & iter > 100) || (!vectorized && iter > 100*n))
+            if(iter > 100)
                 stop('ESRS estimate not stable. Final value of M: ',
                      round(exp(logM), 3))
             logM <- max(c(logM, log_diff + logM))
