6683e29aaf0575b219427fb63f4d4e334b520fae
Phil Chalmers <rphilip.chalmers@gmail.com>
2023-11-15 21:36:49 -0500

    patch predictable artifacts

--- R/PBA.R
+++ R/PBA.R
@@ -197,6 +197,17 @@ PBA <- function(f, interval, ..., p = .6,
 
     for(iter in 1L:maxiter){
         med <- getMedian(fx, x)
+        if(integer && iter > 6L){
+            tmp <- if(replications[iter] == max(replications))
+                medhistory[iter:(iter-3L)-1L] else medhistory[iter:(iter-5L)-1L]
+            if(length(unique(tmp)) == 2L && (max(tmp) - min(tmp)) == 2L)
+                med <- max(tmp) - 1L
+        }
+        if(replications[iter] == max(replications) && integer && iter > 4L){
+            tmp <- medhistory[iter:(iter-3L)-1L]
+            if(length(unique(tmp)) == 2L && (max(tmp) - min(tmp)) == 2L)
+                med <- max(tmp) - 1L
+        }
         medhistory[iter] <- med
         feval <- if(!is.null(FromSimSolve))
             bool.f(f.root=f, med, replications=replications[iter], ...)
