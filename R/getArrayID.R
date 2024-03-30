f5c72e8fc52f2453d793f8acbd9b731617d5a5c9
Phil Chalmers <rphilip.chalmers@gmail.com>
2024-03-30 10:10:02 -0400

    add array ID extractor

---
+++ R/getArrayID.R
@@ -0,0 +1,59 @@
+#' Get job array ID (e.g., from SLURM or other HPC array distributions)
+#'
+#' Get the array ID from an HPC array distribution job.
+#'
+#' The array ID is used to index the rows in the design
+#' object in \code{\link{runSimulation}}. For instance,
+#' a SLURM array with 10 independent jobs might have the following shell
+#' instructions.
+#'
+#' \code{
+#' #!/bin/bash -l
+#' #SBATCH --time=00:01:00
+#' #SBATCH --array=1-10}
+#'
+#' which names the associated jobs with the numbers 1 through 10.
+#' \code{getArrayID()} then extracts this information per array, which
+#' is used as the \code{runSimulation(design, ..., subset = getArrayID())} to
+#' pass specific rows for the \code{design} object.
+#'
+#' @param type an integer indicating the element from the result of
+#'   \code{\link{commandArgs}} to extract, or a \code{character} specifying the
+#'   the type of
+#'
+#' @param trailingOnly logical value passed to \code{\link{commandArgs}}.
+#'   Only used when \code{type} is an integer
+#'
+#' @export
+#'
+#' @seealso \code{\link{runSimulation}} with the \code{subset} argument
+#'
+#' @examples
+#'
+#' \dontrun{
+#'
+#' # get slurm array ID
+#' arrayID <- getArrayID(type = 'slurm')
+#'
+#' # get ID based on first optional argument in shell specification
+#' arrayID <- getArrayID(type = 1)
+#'
+#' # pass as
+#' # runSimulation(design, ...., subset = arrayID)
+#'
+#' }
+#'
+getArrayID <- function(type, trailingOnly = TRUE){
+    stopifnot(length(type) == 1L)
+    ret <- if(is.numeric(type)){
+        args <- commandArgs(trailingOnly = trailingOnly)
+        args[as.integer(type)]
+    } else if(type == 'slurm'){
+        Sys.getenv("SLURM_ARRAY_TASK_ID")
+    } else {
+        stop('type not supported')
+    }
+    ret <- as.integer(ret)
+    if(is.na(ret)) warning("array ID is missing")
+    ret
+}
