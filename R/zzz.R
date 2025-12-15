278c080428195fc48fec5d27e8501f8e080881d0
philchalmers <rphilip.chalmers@gmail.com>
2025-12-15 09:08:27 -0500

    switch to .onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,10 @@
 .onAttach <- function(libname, pkgname) {
+    if(interactive())
+        packageStartupMessage("See ?SimFunctions to get started with SimDesign")
+}
+
+.onLoad <- function(libname, pkgname) {
     # Initialize RNG to ensure .Random.seed exists
     if(!exists(".Random.seed", envir = .GlobalEnv))
         runif(1)
-
-    if(interactive())
-        packageStartupMessage("See ?SimFunctions to get started with SimDesign")
 }
